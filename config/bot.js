82893b9c9bca02830b641b6ddc08cd3cbb6fe19a
ordul <74411150+ordul@users.noreply.github.com>
2021-06-23 03:36:13 +0300

    asdsadasd
    asd

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'NzQxMjMwMTkxODAzNzYwNjcx.Xy0iaQ.y7_7-AUBz50_GN7AZlmMDyJV1ME',
+        prefix: '.',
+        activity: 'stop',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
